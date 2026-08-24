72e7ae22c98bf637aa1e2b26d1e37d88277678fe
switchblade <rajdipsaha31@gmail.com>
2026-08-25 03:03:59 +0530

    Keep resource accessor namespace intact in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -39,7 +39,7 @@ val coreDir = File(rootDir, "core")
 val serviceDir = File(rootDir, "core/service")
 val mediaDir = File(rootDir, "core/media")
 
-rootProject.name = "PalmPlayer"
+rootProject.name = "EchoMusic"
 include(
     ":androidApp",
     ":composeApp",
